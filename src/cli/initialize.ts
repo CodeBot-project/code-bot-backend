f8c1e58e2769da48bc4a5ce1367141c902e10430
IrisTheSnail <salwa999adlouni@gmail.com>
2023-06-01 22:13:44 +0100

    Added: OpenAI api wrapper

--- src/cli/initialize.ts
+++ src/cli/initialize.ts
@@ -1,4 +1,5 @@
 import * as config from '../core/config/config'
+import { askChatGPT } from '../core/gpt/api'
 import { SimplePromptParser } from '../core/gpt/prompt/SimplePromptParser'
 import { parseInput } from '../core/input/input'
 
